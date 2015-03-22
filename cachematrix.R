d65790ba44fa3906feb4178e6f26dc50737e3c20
Szabados Levente <levente.szabados@analogy.co>
2015-03-22 13:02:28 +0100

    programming assignment 2 solution commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an implementation of the Coursera "Introduction to R" course assignment 2
+## The goal is to make a caching function set for matrix inversion
 
-## Write a short comment describing this function
-
+## This function optionally receives an existing matrix or creates a new one,
+## and provides get and set functionalities as a frame for accessing cached data
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NaN
+  	set <- function(y) {
+                x <<- y
+                inv <<- NaN
+        }
+        get <- function() x
+        setinverse <- function(sol) inv <<- sol
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
-
+## This is the main function that serves as a cached calculator of matrix inversion
+## It receives a matrix, checks if there is already an inverse cached, if yes, gives back the cached results with an appropriate message,
+## if no, calculates the inverse, stores it into the cache and gives it back.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inve <- x$getinverse()
+        if(!is.nan(inve)) {
+                message("getting cached data")
+                return(inve)
+        }
+        data <- x$get()
+        inve <- solve(data, ...)
+        x$setinverse(inve)
+        inve
 }
+
+
+
+
